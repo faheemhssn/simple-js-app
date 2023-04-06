1045eb3d831293f6c136e7a7c86510c81b58f8c3
Faheem Hassan <faheemhssn@gmail.com>
2023-04-06 16:14:42 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,7 @@ let pokemonRepository = (function () {
     { name: 'Venusaur', height: 2, weight: 100, types: ['grass', 'poison']},
     { name: 'Charizard', height: 1.7, weight: 90.5, types: ['monster', 'dragon']},
     { name: 'Blastoise', height: 1.6, weight: 88.5, types: ['monster', 'water']},
-    { name: 'Raichu', height: 0.8, weight: 30, types: ['field', 'fairy']},
-  ];
+   ];
 
   function add(pokemon) {
     pokemonList.push(pokemon);
