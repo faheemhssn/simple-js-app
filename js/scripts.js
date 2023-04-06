c3421c44643e4e6ebf45551234f0e5abfcf177d4
faheemhssn <faheemhssn@gmail.com>
2023-04-06 14:29:11 -0400

    Added pokemon attributes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
-//Used pokedex.org for pokemon data
-
 let pokemonRepository = (function() {
   let pokemonList = [
-      {name:'Bulbasaur', height:'0.7', type:['Grass', 'Poison']},
-      {name:'Ivysaur', height:'1', type: ['Grass', 'Poison']},
-      {name:'Venusaur', height:'2', type: ['Grass', 'Poison']},
+      {name:'Bulbasaur', height:'0.7', weight: '6.9', type:['Grass', 'Poison']},
+      {name:'Ivysaur', height:'1', weight: 13, type: ['Grass', 'Poison']},
+      {name:'Venusaur', height:'2', weight:'100', type: ['Grass', 'Poison']},
     ]})
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+
+
+pokemonRepository.getAll().forEach(function(pokemon){ 
+  document.write('<p>' + pokemon.name + " Height:" + pokemon.height + "m Weight:" + pokemon.weight + " Types:" + pokemon.types)
+})
